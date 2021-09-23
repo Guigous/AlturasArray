de21de633f715fbf4c0211f258595bb85f14c857
Guigous <gui_1810@hotmail.com>
2021-09-23 00:07:08 -0300

    Codigo comentado SHOW

---
+++ Alturas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Alturas
+{
+    class Program
+    {
+        
+
+
+        static void Main(string[] args)
+        {
+            //Input de dados
+            Console.WriteLine("Digite o Numero de pessoas : ");
+            int x= int.Parse(Console.ReadLine());
+            double[] Doublearray = new double[x];
+            
+            //Loop
+            for (int i=0;i<x;i++)
+
+            {
+                //Input de Dados
+                Console.WriteLine("Digite As Alturas : ");
+                double n = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture );
+                Doublearray[i] = n;
+            }
+            //Resultado          
+            double a = Doublearray.Sum();
+            Console.WriteLine(a/x);
+            
+        }
+    }
+}
+
